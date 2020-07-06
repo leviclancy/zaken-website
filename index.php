79d2e722139b6efa2d30b4c9df4f60e3620a1a4f
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-07-06 11:40:02 -0700

    Update index.php

--- index.php
+++ index.php
@@ -9,91 +9,39 @@
 /// Contact
 
 $sitemap_array = [
-  "home" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"",
-		],
-  "books" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Bookstore",
-		],
-  "kurdistan" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Resources about Kurdistan",
-		],
-  "israeli-arabs" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Resources about Israeli Arabs",
-		],
-  "israel-and-zionism" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Israel and Zionism",
-		],
-$sitemap_array = [
-  "home" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"",
-		],
-  "books" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Bookstore",
-		],
-  "kurdistan" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Resources about Kurdistan",
-		],
-  "israeli-arabs" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Resources about Israeli Arabs",
-		],
-  "booking" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Booking",
-		],
-$sitemap_array = [
-  "home" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"",
-		],
-  "books" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Bookstore",
-		],
-  "kurdistan" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Resources about Kurdistan",
-		],
-  "israeli-arabs" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Resources about Israeli Arabs",
-		],
-  "contact" => [
-		"he"=> "",
-		"ku"=>"",
-		"en"=>"Contact",
-		],
+"home" => [
+	"he"=> "",
+	"ku"=>"",
+	"en"=>"",
+	],
+"bookstore" => [
+	"he"=> "",
+	"ku"=>"",
+	"en"=>"Bookstore",
+	],
+"kurdistan" => [
+	"he"=> "",
+	"ku"=>"",
+	"en"=>"Resources about Kurdistan",
+	],
+"israeli-arabs" => [
+	"he"=> "",
+	"ku"=>"",
+	"en"=>"Resources about Israeli Arabs",
+	],
+"israel-and-zionism" => [
+	"he"=> "",
+	"ku"=>"",
+	"en"=>"Israel and Zionism",
+"contact" => [
+	"he"=> "",
+	"ku"=> "",
+	"en"=> "Contact",
 	];
-
 
 // Kurdistan
 // https://www.youtube.com/watch?v=UkSRVefP1Qw at Jewish World Congress
 // https://www.youtube.com/watch?v=9FODj66nvkk  at JWC
 // /media/mulla-mustafa-moshe-dayan.jpg
 	
-
 ?>
-
