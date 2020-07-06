267d0369c03edfd6081709e6ef8067fc6da1a250
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-07-06 11:38:34 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,99 @@
 <?
 
 /// Home page
-/// Books
+/// Bookstore
 /// Resources about Kurdistan
 /// Resources about Israeli Arabs
 /// Resources about Israel and Zionism
 /// Booking
 /// Contact
 
+$sitemap_array = [
+  "home" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"",
+		],
+  "books" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Bookstore",
+		],
+  "kurdistan" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Resources about Kurdistan",
+		],
+  "israeli-arabs" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Resources about Israeli Arabs",
+		],
+  "israel-and-zionism" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Israel and Zionism",
+		],
+$sitemap_array = [
+  "home" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"",
+		],
+  "books" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Bookstore",
+		],
+  "kurdistan" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Resources about Kurdistan",
+		],
+  "israeli-arabs" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Resources about Israeli Arabs",
+		],
+  "booking" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Booking",
+		],
+$sitemap_array = [
+  "home" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"",
+		],
+  "books" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Bookstore",
+		],
+  "kurdistan" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Resources about Kurdistan",
+		],
+  "israeli-arabs" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Resources about Israeli Arabs",
+		],
+  "contact" => [
+		"he"=> "",
+		"ku"=>"",
+		"en"=>"Contact",
+		],
+	];
+
+
+// Kurdistan
+// https://www.youtube.com/watch?v=UkSRVefP1Qw at Jewish World Congress
+// https://www.youtube.com/watch?v=9FODj66nvkk  at JWC
+// /media/mulla-mustafa-moshe-dayan.jpg
+	
+
 ?>
+
