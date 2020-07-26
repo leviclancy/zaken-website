681fbab1f56dc6961696309e7db679fe1e10bd00
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-07-26 12:21:54 -0700

    Update and rename translatable-elements.json to translatable-elements.php

---
+++ translatable-elements.php
@@ -0,0 +1,52 @@
+$translatable_elements = [
+	
+	"from-the-desk-of" => [
+		"ar"	=> "",
+		"en"	=> "From the desk of",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+		
+	"dr-mordechai-zaken" => [
+		"ar"	=> "",
+		"en"	=> "Dr. Mordechai Zaken",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+		
+	"home" => [
+		"ar"	=> "",
+		"en"	=> "Home",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+
+	"bookstore" => [
+		"ar"	=> "",
+		"en"	=> "Bookstore",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+
+	"kurdistan" => [
+		"ar"	=> "",
+		"en"	=> "Kurdistan",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+
+	"israeli-arabs" => [
+		"ar"	=> "",
+		"en"	=> "Israeli Arabs",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+		
+	"israel-and-zionism" => [
+		"ar"	=> "",
+		"en"	=> "Israel + Zionism",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+
+	];
