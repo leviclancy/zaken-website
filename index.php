aaff99959557eb82902db6043d248971742917f5
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-07-29 20:23:40 -0700

    Update index.php

--- index.php
+++ index.php
@@ -143,7 +143,7 @@ $style_array = [
 //		"background"		=> "rgba(100,100,100,1)",
 //		"color"			=> "rgba(255,255,255,0.65)",
 		"font-weight"		=> "700",
-		"text-shadow"		=> "3px 3px 25px rgba(50,50,50,0.25)",
+		"text-shadow"		=> "3px 3px 10px rgba(50,50,50,0.3)",
 		],
 	
 	"#body-content" => [
