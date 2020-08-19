d4bced78328d3583e7c8c6917769ac58044cb622
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-08-19 12:29:51 -0700

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -76,5 +76,22 @@
 		"he"	=> "",
 		"ku"	=> "",
 		],
+	
+	"who-is-the-representative-question" => [
+		"ar"	=> "",
+		"en"	=> "Who is the representative for Jewish affairs and Jewish sites in the Kurdistan Region?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"who-is-the-representative-answer" => [
+		"ar"	=> "",
+		"en"	=> "Dr. Mordechai Zaken is the representative appointed by the National Association of Jews of Kurdistan in Israel regarding Jewish affairs and Jewish sites in the Kurdistan Region, and he also serves as National Association's cultural and historical expert of the Jewish Kurdish history and heritage. He was nominated by the National Association, and the National Association has communicated twice with the Kurdistan Region President's bureau in the KRG through two letters that Dr. Zaken and the National Association's National Chair Ammon Ashkenazi sent expressing their concern regarding acts such as taking over or abusing the Tomb of Nahum by politicians and others. It was agreed that the Kurdistan Region would be glad to be advised by Dr. Zaken in such matters related to the Jewish community and heritage, pending officialization which is on hold due to the cornavirus pandemic.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	
+	
 
 	]; ?>
