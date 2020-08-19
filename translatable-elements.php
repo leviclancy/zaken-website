c45ff2d8bf5ff6951a7bea3035a39e1ee82e8d98
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-08-19 13:01:37 -0700

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -106,8 +106,8 @@
 		],
 
 	"what-is-the-scope-question" => [
-		"ar"	=> "What is the scope of the role of representative?",
-		"en"	=> "",
+		"ar"	=> "",
+		"en"	=> "What is the scope of the role of representative?",
 		"he"	=> "",
 		"ku"	=> "",
 		],
@@ -147,6 +147,159 @@
 		"ku"	=> "",
 		],
 	
+	"how-was-dr-zaken-appointed-question" => [
+		"ar"	=> "",
+		"en"	=> "How was Dr. Zaken appointed?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"how-was-dr-zaken-appointed-answer" => [
+		"ar"	=> "",
+		"en"	=> "Dr. Zaken's parents are both Kurdish Jews, and his father was born in Zakho, in the Kurdistan Region. His career has been devoted to Kurdish Jews, and he is a scholarly and community leader on this topic.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-is-the-population-question" => [
+		"ar"	=> "",
+		"en"	=> "What is the population of Jews from Kurdistan?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-is-the-population-answer" => [
+		"ar"	=> "",
+		"en"	=> "There are approximately 200,000 Jews who are ancestrally from Kurdistan. The community of Jews from Kurdistan has maintained continuous rabbinical, social, and other institutions from the time they were in Kurdistan through today.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"are-there-jews-in-kurdistan-question" => [
+		"ar"	=> "",
+		"en"	=> "Are there Jews in Kurdistan?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"are-there-jews-in-kurdistan-answer" => [
+		"ar"	=> "",
+		"en"	=> "To be born Jewish, someone must have a Jewish mother as recognized by a Rabbi. There are zero Jews remaining in the Kurdistan Region, except for a small number of expatriates.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-about-the-impostors-question" => [
+		"ar"	=> "",
+		"en"	=> "What about impostors in Kurdistan, claiming to be Jewish in the news?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-about-the-impostors-answer" => [
+		"ar"	=> "",
+		"en"	=> "As stated above, there are zero Jews remaining in the Kurdistan Region. Recently, some eccentric Muslims have claimed to be Jewish. This is a total fraud, and a deeply antisemitic betrayal of Judaism, Jewish people, and Jewish values. When confronted, they are hostile and totally indistinguishable from any other types of antisemites.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"who-does-the-representative-not-represent-question" => [
+		"ar"	=> "",
+		"en"	=> "Who does the representative <i>not</i> represent?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"who-does-the-representative-not-represent-answer" => [
+		"ar"	=> "",
+		"en"	=> "The representative is not representing the governments of Israel nor the Kurdistan Region in this role. It is an appointment made by the civil and religious leadership of the Jews of Kurdistan, and the community's desired leadership is respectfully recognized by the Ministry of Endowments and Religious Affairs in the Kurdistan Region.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-does-the-representative-not-represent-question" => [
+		"ar"	=> "",
+		"en"	=> "What issues does the representative <i>not</i> represent?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-does-the-representative-not-represent-answer" => [
+		"ar"	=> "",
+		"en"	=> "As an appointment made by the National Association of Jews from Kurdistan and recognized by the Ministry of Endowments of Religious Affairs in the Kurdistan Region, the representative is not involved in issues beyond the authority of these institutions. The role of represenative does not include bilateral relations between any two countries, nor is the role of representative devoted in any way to broader issues such as infrastructure.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"will-the-jews-from-kurdistan-return-question" => [
+		"ar"	=> "",
+		"en"	=> "Will the Jews from Kurdistan return?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"will-the-jews-from-kurdistan-return-answer" => [
+		"ar"	=> "",
+		"en"	=> "Not really. Returning would mean leaving behind relatives, cherished institutions, and a present-day identity. However, it is good to expect pilgrims. If even three or four families were to return to the Kurdistan Region, find work, and buy homes that would be incredible.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"can-i-be-part-jewish-question" => [
+		"ar"	=> "",
+		"en"	=> "Can I be part Jewish?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"can-i-be-part-jewish-answer" => [
+		"ar"	=> "",
+		"en"	=> "There are many people with distant Jewish relatives who converted to Islam and Christianity, and intermarried. They are not Jewish. However, well-intentioned interest in learning about their family history is a welcomed and personal basis for enhancing mutual respect between Jewish and non-Jewish communities.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"can-i-visit-israel-question" => [
+		"ar"	=> "",
+		"en"	=> "Can I visit Israel?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"can-i-visit-israel-answer" => [
+		"ar"	=> "",
+		"en"	=> "Iraq does not recognize Israel's existence. This makes it almost impossible for Israel to grant visas to Iraqi passport holders. However, Israel has remained committed to providing exceptions for international organizations that bring Iraqis to Israel for life-saving medical treatments.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"are-there-exchanges-question" => [
+		"ar"	=> "",
+		"en"	=> "Are there academic, cultural, and other exchanges?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"are-there-exchanges-answer" => [
+		"ar"	=> "",
+		"en"	=> "If you are interested in arranging exchanges online, we are happy to encourage projects that bring students in the Kurdistan Region together with Jewish students outside the Kurdistan Region.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-if-i-want-to-be-jewish-question" => [
+		"ar"	=> "",
+		"en"	=> "What if I want to be Jewish or consider myself Jewish?",
+		"he"	=> "",
+		"ku"	=> "",
+		],
+	
+	"what-if-i-want-to-be-jewish-answer" => [
+		"ar"	=> "",
+		"en"	=> "Jewish status is based on recognition by a Rabbi. There are no Jews remaining in the Kurdistan Region, except for expatriates. The community of Jews from Kurdistan is not at all interested in conversion.",
+		"he"	=> "",
+		"ku"	=> "",
+		],
 	"-question" => [
 		"ar"	=> "",
 		"en"	=> "",
@@ -160,63 +313,7 @@
 		"he"	=> "",
 		"ku"	=> "",
 		],
-	
-	"-question" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-answer" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-question" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-answer" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-question" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-answer" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-question" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-answer" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
+
 	
 	
 
