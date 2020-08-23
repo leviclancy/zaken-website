5189f6a65055cfd09493be77233dd6d8522d2f26
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-08-23 00:21:25 -0700

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -1,8 +1,8 @@
 <? $translatable_elements = [
 	
-	"from-the-desk-of" => [
+	"the-official-website-of" => [
 		"ar"	=> "",
-		"en"	=> "From the desk of",
+		"en"	=> "The official website of",
 		"he"	=> "",
 		"ku"	=> "",
 		],
@@ -35,27 +35,20 @@
 		"ku"	=> "",
 		],
 
-	"jews-of-kurdistan" => [
+	"kurdistan-region" => [
 		"ar"	=> "",
-		"en"	=> "Jews of Kurdistan",
+		"en"	=> "Kurdistan Region",
 		"he"	=> "",
 		"ku"	=> "",
 		],
 
-	"arab-and-minority-affairs" => [
+	"israel" => [
 		"ar"	=> "",
-		"en"	=> "Arab and Minority Affairs",
+		"en"	=> "Israel",
 		"he"	=> "",
 		"ku"	=> "",
 		],
 		
-	"israel-and-zionism" => [
-		"ar"	=> "",
-		"en"	=> "Israel and Zionism",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-
 	"contact" => [
 		"ar"	=> "",
 		"en"	=> "Contact",
@@ -76,245 +69,6 @@
 		"he"	=> "",
 		"ku"	=> "",
 		],
-	
-	"who-is-the-representative-question" => [
-		"ar"	=> "",
-		"en"	=> "Who is the representative for Jewish affairs and Jewish sites in the Kurdistan Region?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"who-is-the-representative-answer" => [
-		"ar"	=> "",
-		"en"	=> "Dr. Mordechai Zaken is the representative appointed by the National Association of Jews of Kurdistan in Israel regarding Jewish affairs and Jewish sites in the Kurdistan Region, and he also serves as National Association's cultural and historical expert of the Jewish Kurdish history and heritage. He was nominated by the National Association, and the National Association has communicated twice with the Kurdistan Region President's bureau in the KRG through two letters that Dr. Zaken and the National Association's National Chair Ammon Ashkenazi sent expressing their concern regarding acts such as taking over or abusing the Tomb of Nahum by politicians and others. It was agreed that the Kurdistan Region would be glad to be advised by Dr. Zaken in such matters related to the Jewish community and heritage, pending officialization which is on hold due to the cornavirus pandemic.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-is-the-role-question" => [
-		"ar"	=> "",
-		"en"	=> "What is the role of representative?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-is-the-role-answer" => [
-		"ar"	=> "",
-		"en"	=> "The Jews of Kurdistan are happy to share hands with the Kurdistan Region's authorities, Muslim Kurdish leaders, and Assyrian and Chaldean communities in alQosh to protect and burnish religious and spiritual sites such as the Tomb of Nahum and others, in the name of guarding sites from damage and abuse. The representative supports reconciliation, coexistence, and mutual positivity for all.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-
-	"what-is-the-scope-question" => [
-		"ar"	=> "",
-		"en"	=> "What is the scope of the role of representative?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-is-the-scope-answer" => [
-		"ar"	=> "",
-		"en"	=> "The scope of the role of representative is regarding Jewish heritage in areas administered by the Kurdistan Region.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-
-	"who-does-the-representative-represent-question" => [
-		"ar"	=> "",
-		"en"	=> "Who does the representative represent?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"who-does-the-representative-represent-answer" => [
-		"ar"	=> "",
-		"en"	=> "The representative represents the community of Jews from Kurdistan, with regards to issues within the jurisdiction of the Ministry of Endowments and Religious Affairs in the Kurdistan Region. Although there are no Jews in the Kurdistan Region except for some expatriates, there is Jewish heritage and goodwill towards Jews in the Kurdistan Region which is of deep importance to the community of Jews from Kurdistan.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"how-is-the-representative-appointed-question" => [
-		"ar"	=> "",
-		"en"	=> "How is the representative appointed?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"how-is-the-representative-appointed-answer" => [
-		"ar"	=> "",
-		"en"	=> "The role of representative is selected by the National Association of Jews from Kurdistan. Authority is vested in the representative by the National Association, and recognized by the Ministry of Endowments and Religious Affairs in the Kurdistan Region.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"how-was-dr-zaken-appointed-question" => [
-		"ar"	=> "",
-		"en"	=> "How was Dr. Zaken appointed?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"how-was-dr-zaken-appointed-answer" => [
-		"ar"	=> "",
-		"en"	=> "Dr. Zaken's parents are both Kurdish Jews, and his father was born in Zakho, in the Kurdistan Region. His career has been devoted to Kurdish Jews, and he is a scholarly and community leader on this topic.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-is-the-population-question" => [
-		"ar"	=> "",
-		"en"	=> "What is the population of Jews from Kurdistan?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-is-the-population-answer" => [
-		"ar"	=> "",
-		"en"	=> "There are approximately 200,000 Jews who are ancestrally from Kurdistan. The community of Jews from Kurdistan has maintained continuous rabbinical, social, and other institutions from the time they were in Kurdistan through today.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"are-there-jews-in-kurdistan-question" => [
-		"ar"	=> "",
-		"en"	=> "Are there Jews in Kurdistan?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"are-there-jews-in-kurdistan-answer" => [
-		"ar"	=> "",
-		"en"	=> "To be born Jewish, someone must have a Jewish mother as recognized by a Rabbi. There are zero Jews remaining in the Kurdistan Region, except for a small number of expatriates. There is no Jewish community in Kurdistan – no synagogues, no Jewish activity whatsoever. There are hundreds of Kurds with Israeli passports in Kurdistan, and even some who know Hebrew, but these are Muslim Kurds – most of whom came to Israel for several years during the immigration fiasco of the 1990s. But when things improved in Kurdistan, they returned with their Israeli passports or moved to Europe. One Jewish grandparent bringing an extended family to Israel could mean as many as 30 Kurdish Muslims per grandparent. These people had no intention of becoming Jews. They attended mosques while in Israel. A tiny number expressed interest in converting to Judaism, but completely failed to do so without any exceptions.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-about-the-impostors-question" => [
-		"ar"	=> "",
-		"en"	=> "What about impostors in Kurdistan, claiming to be Jewish in the news?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-about-the-impostors-answer" => [
-		"ar"	=> "",
-		"en"	=> "As stated above, there are zero Jews remaining in the Kurdistan Region. Recently, some eccentric Muslims have claimed to be Jewish. This is a total fraud, and a deeply antisemitic betrayal of Judaism, Jewish people, and Jewish values. When confronted, they are hostile and totally indistinguishable from any other types of antisemites.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"who-does-the-representative-not-represent-question" => [
-		"ar"	=> "",
-		"en"	=> "Who does the representative <i>not</i> represent?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"who-does-the-representative-not-represent-answer" => [
-		"ar"	=> "",
-		"en"	=> "The representative is not representing the governments of Israel nor the Kurdistan Region in this role. It is an appointment made by the civil and religious leadership of the Jews of Kurdistan, and the community's desired leadership is respectfully recognized by the Ministry of Endowments and Religious Affairs in the Kurdistan Region.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-does-the-representative-not-represent-question" => [
-		"ar"	=> "",
-		"en"	=> "What issues does the representative <i>not</i> represent?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-does-the-representative-not-represent-answer" => [
-		"ar"	=> "",
-		"en"	=> "As an appointment made by the National Association of Jews from Kurdistan and recognized by the Ministry of Endowments of Religious Affairs in the Kurdistan Region, the representative is not involved in issues beyond the authority of these institutions. The role of represenative does not include bilateral relations between any two countries, nor is the role of representative devoted in any way to broader issues such as infrastructure.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"will-the-jews-from-kurdistan-return-question" => [
-		"ar"	=> "",
-		"en"	=> "Will the Jews from Kurdistan return?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"will-the-jews-from-kurdistan-return-answer" => [
-		"ar"	=> "",
-		"en"	=> "Not really. Returning would mean leaving behind relatives, cherished institutions, and a present-day identity. However, it is good to expect pilgrims. If even three or four families were to return to the Kurdistan Region, find work, and buy homes that would be incredible.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"can-i-be-part-jewish-question" => [
-		"ar"	=> "",
-		"en"	=> "Can I be part Jewish?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"can-i-be-part-jewish-answer" => [
-		"ar"	=> "",
-		"en"	=> "There are many people with distant Jewish relatives who converted to Islam and Christianity, and intermarried. They are not Jewish. However, well-intentioned interest in learning about their family history is a welcomed and personal basis for enhancing mutual respect between Jewish and non-Jewish communities.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"can-i-visit-israel-question" => [
-		"ar"	=> "",
-		"en"	=> "Can I visit Israel?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"can-i-visit-israel-answer" => [
-		"ar"	=> "",
-		"en"	=> "Iraq does not recognize Israel's existence. This makes it almost impossible for Israel to grant visas to Iraqi passport holders. However, Israel has remained committed to providing exceptions for international organizations that bring Iraqis to Israel for life-saving medical treatments.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"are-there-exchanges-question" => [
-		"ar"	=> "",
-		"en"	=> "Are there academic, cultural, and other exchanges?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"are-there-exchanges-answer" => [
-		"ar"	=> "",
-		"en"	=> "If you are interested in arranging exchanges online, we are happy to encourage projects that bring students in the Kurdistan Region together with Jewish students outside the Kurdistan Region.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-if-i-want-to-be-jewish-question" => [
-		"ar"	=> "",
-		"en"	=> "What if I want to be Jewish or consider myself Jewish?",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"what-if-i-want-to-be-jewish-answer" => [
-		"ar"	=> "",
-		"en"	=> "Jewish status is based on recognition by a Rabbi. There are no Jews remaining in the Kurdistan Region, except for expatriates. The community of Jews from Kurdistan is not at all interested in conversion.",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	"-question" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-	
-	"-answer" => [
-		"ar"	=> "",
-		"en"	=> "",
-		"he"	=> "",
-		"ku"	=> "",
-		],
-
-	
 	
 
 	]; ?>
