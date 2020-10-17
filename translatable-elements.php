d2cd962ddf1e51feb82e9fd68ee8d5bf4111fe03
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-10-17 22:08:30 +0300

    Update translatable-elements.php

--- translatable-elements.php
+++ translatable-elements.php
@@ -8,7 +8,7 @@
 		],
 	
 	"historian-expert-and-author" => [
-		"en"	=> "Historian, expert, and author on the Jewish and Assyrian minorities and the Kurdish society",
+		"en"	=> "Historian and Expert on the Jews of Kurdistan, the Assyrian minorities, and the tribal Kurdish society",
 		"he"	=> "",
 		"ar"	=> "",
 		"ku"	=> "",
@@ -56,15 +56,15 @@
 		"ku"	=> "",
 		],
 
-	"publications" => [
-		"en"	=> "Publications",
+	"publications-and-lectures" => [
+		"en"	=> "Publications and Lectures",
 		"he"	=> "",
 		"ar"	=> "",
 		"ku"	=> "",
 		],
 
-	"publications" => [
-		"en"	=> "Publications",
+	"lectures" => [
+		"en"	=> "Lectures",
 		"he"	=> "",
 		"ar"	=> "",
 		"ku"	=> "",
@@ -77,13 +77,6 @@
 		"ku"	=> "",
 		],
 	
-	"full-bibliography" => [
-		"en"	=> "Full Bibliography",
-		"he"	=> "",
-		"ar"	=> "",
-		"ku"	=> "",
-		],
-
 	"kurdistan-region" => [
 		"en"	=> "Kurdistan Region",
 		"he"	=> "",
@@ -98,8 +91,8 @@
 		"ku"	=> "",
 		],
 
-	"historical-information" => [
-		"en"	=> "Historical Information",
+	"kurdistan-region-and-israel" => [
+		"en"	=> "Kurdistan Region and Israel",
 		"he"	=> "",
 		"ar"	=> "",
 		"ku"	=> "",
@@ -126,20 +119,209 @@
 		"ku"	=> "",
 		],
 		
-	"press" => [
+	"press-and-bookings" => [
 		"en"	=> "Press and Bookings",
 		"he"	=> "",
 		"ar"	=> "",
 		"ku"	=> "",
 		],
 	
-	"press-history" => [
-		"en"	=> "Press History",
+	"biographical-notes-in-erbil" => [
+		"en"	=> "Dr. Zaken in Erbil, Kurdistan, Oct. 2012.",
 		"he"	=> "",
 		"ar"	=> "",
 		"ku"	=> "",
 		],
-		
+	"biographical-notes-dr-mordechai-zaken-born" => [
+		"en"	=> "Dr. Mordechai Zaken, born in Jerusalem of Kurdistani Jewish descent, is an eminent and authoritative historian of the Jews of Kurdistan, as well as the native Assyrians and the tribal Kurdish society. In 2019, he received the Prime Minister’s Prize for his research and leadership.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"biographical-notes-authority" => [
+		"en"	=> "Authority on the history and heritage of the Jews of Kurdistan, the Assyrians, and the tribal Kurdish society",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"biographical-notes-representative" => [
+		"en"	=> "Representative of the community of Jews from Kurdistan to the Kurdistan Region (2020 - ongoing)",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+	
+	"biographical-notes-author" => [
+		"en"	=> "Author of ‘The Jews of Kurdistan’",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"biographical-notes-adviser" => [
+		"en"	=> "Adviser on Arab Affairs to the Prime Minister of Israel",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-head" => [
+		"en"	=> "Head of Minority Affairs Desk at Israel's Ministry of Public Security",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-vidoe-screened" => [
+		"en"	=> "Video screened at the official ceremony in Jerusalem, in March 2019, when Dr. Zaken’s was awarded the Prime Minister’s Prize for research on the Jews of the Middle East and Iran. Transcript here: <a href='https://docs.google.com/document/d/1C7RHxNWgn13X9jm_dFQClStACOqCygpKgTVC4342bCA/edit?usp=sharing'>doc.google.com</a>",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-uzi-dayan" => [
+		"en"	=> "Moti Zaken (right) with his friend, the decorated General (Reserve) Uzi Dayan, a staunch supporter of the Kurds, at the courtyard Cafe of the Christ-Church in East Jerusalem.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-doctoral-thesis" => [
+		"en"	=> "Dr. Zaken’s doctoral thesis, and the books which resulted from it, were praised by remarkable scholars such as Joyce Blau of Paris’ INALCO and Moshe Sharon of Jerusalem’s Hebrew University for skillfully using careful oral history documentation of the Jews of Kurdistan and for saving the oral history of the Jews of Kurdistan from being lost forever. His thesis and the books that followed became a leading resource on the topic, because it was compiled from hundreds of interviews with native Kurdish Jews who immigrated to Israel, and because Dr. Zaken’s research was widely spread and translated into several languages including Arabic, Kurdish, and French.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-also-expresses" => [
+		"en"	=> "Dr. Zaken also expresses gratitude to teachers and scholars who inspired him such as the late Gideon Goldenberg and Amnon Netzer, and Benjamin Z. Kedar, Martin van Bruinessen, and Michael Chyet.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-bedouin-communities" => [
+		"en"	=> "Dr. Zaken, as the Prime Minister’s adviser on Arab affairs, with Prime Minister Benjamin Netanyahu, during a tour in the Negev to cities and un-sedentary encampments of Bedouin communities (1998).",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-speaks-fluently" => [
+		"en"	=> "Dr. Mordechai Zaken (also called by his nickname, Moti) speaks fluently the Eastern Neo-Aramaic dialects spoken by the Jews of Kurdistan, which resembles the dialects spoken by the native Assyrian Christians today. He also speaks Arabic, English, and his mother tongue, Hebrew. In addition, he has practical knowledge of Farsi, Kurmanji, and French. He lives in Jerusalem with his wife and their three children.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+
+	"biographical-notes-website-administration" => [
+		"en"	=> "Website administration",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"biographical-notes-this-website" => [
+		"en"	=> "This website has been developed by Foundation of Ours and has been brought to the public in multiple languages in alignment with the Foundation's mission to support Jewish expression in the Kurdistan Region.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"biographical-notes-thanks" => [
+		"en"	=> "The Foundation thanks Dr. Zaken for his cooperation and supervision, and the freedom to choose and select material and photos, as part of a mutual hope of supporting reconciliation between Jews and Muslims of Kurdish origin.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"scholarly-achievement-usually" => [
+		"en"	=> "Usually Dr. Zaken interviewed his Jewish informants at their homes. At times, he met them at the synagogue, or at a community center. He interviewed some of his informants on a bench near their synagogue, where they used to hang out during the afternoon, before the evening prayer. In the photo above, Dr. Zaken (right) interviews Muallem Levi (center) of Zakho, a frequent visitor to his parents’ home. To the left is Dr. Zaken’s father, Saleh, who was also born in Zakho. Circa 1987.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"scholarly-achievement-became-curious" => [
+		"en"	=> "When Mordechai Zaken became curious about the Jews of Kurdistan, during the early 1980s, he was puzzled to see that there was hardly any written material.",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
+	"" => [
+		"en"	=> "",
+		"he"	=> "",
+		"ar"	=> "",
+		"ku"	=> "",
+		],
+
 	"contact" => [
 		"en"	=> "Contact",
 		"he"	=> "",
