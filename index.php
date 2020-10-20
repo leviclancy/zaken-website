836671b09f5a6b6e97cd49b1b6f46eafed2fcc7a
Levi Clancy <32032952+leviclancy@users.noreply.github.com>
2020-10-20 23:36:27 +0300

    Update index.php

--- index.php
+++ index.php
@@ -428,7 +428,7 @@ function blockquote($content_id, $attribution_id=null) {
 	global $language_request;
 	if (empty($content_id)): return; endif;
 	$string_temp = "<blockquote>". translatable_elements($content_id) ."";
-	if (!(empty($attribution_id))): $string_temp .= " &nsbp;&nsbp;&nsbp; <i>". translatable_elements($attribution_id) ."</i>"; endif;
+	if (!(empty($attribution_id))): $string_temp .= " &nbsp;&nbsp;&nbsp; <i>". translatable_elements($attribution_id) ."</i>"; endif;
 	$string_temp .= "</blockquote>";
 	return $string_temp; }
 
